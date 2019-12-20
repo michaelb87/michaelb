31a1d765b319455076430ee1436af47c32d62e6d
Michael Bierbaumer <mb@michaelb.at>
2019-12-20 22:14:35 +0100

    added frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <section class="section test-theme">
+      <h1 class="hello-nuxt">Hello world :)</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
