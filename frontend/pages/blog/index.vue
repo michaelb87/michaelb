3d809b3ed203c5eede0633d0eab279968af2af7a
Michael Bierbaumer <mb@michaelb.at>
2019-12-21 00:12:05 +0100

    added graphql categories in frontend

--- frontend/pages/blog/index.vue
+++ frontend/pages/blog/index.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="container">
     <section class="section test-theme">
-      <h1 class="hello-nuxt">Blog</h1>
+      <h1 class="title">Blog</h1>
+      <span v-for="category in categories" :key="category.name">{{category.name}}</span>
     </section>
   </div>
 </template>
+
+<script>
+import categoriesQuery from '~/apollo/queries/category/categories.gql';
+
+export default {
+    data() {
+        return {
+            categories: []
+        }
+    },
+    apollo: {
+        categories: {
+        prefetch: true,
+        query: categoriesQuery
+    }
+  }
+}
+</script>
