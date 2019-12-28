b676e043cc92c264fcc287548a50aeae36ad39ab
Michael Bierbaumer <mb@michaelb.at>
2019-12-28 10:47:53 +0100

    about page update

--- frontend/components/ImgModal.vue
+++ frontend/components/ImgModal.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <img :src="src" @click="isActive = true" />
+    <img :src="src" @click="isActive = true" class="pointer-cursor" v-bind:class="clazz" role="button" />
     <div class="modal is-active" v-if="isActive">
       <div class="modal-background"></div>
       <div class="modal-content" v-on-clickaway="deActivate">
-        <p class="image is-4by3">
+        <p class="image">
           <img :src="src" alt />
         </p>
       </div>
@@ -15,7 +15,7 @@
 <script>
 import { mixin as clickaway } from "vue-clickaway";
 export default {
-  props: ["src"],
+  props: ["src", "clazz"],
   mixins: [clickaway],
   data: function() {
     return {
--- frontend/pages/about/index.vue
+++ frontend/pages/about/index.vue
@@ -1,54 +1,46 @@
 <template>
   <div class="container">
     <section class="section">
-      <h4 class="subtitle is-4">If you'd like to know more</h4>
-      <div>
-        So my name is Michael Bierbaumer, a {{age.years()}} years old freelance software engineer, living in Austria right in the center of Europe 
-        <span class="icon icon-txt-align">
-          <img class="" src="~/assets/icons/eu.svg" />
-        </span> 
-      </div>
-      <div>
-        After living 11 years in Vienna - the capital city of Austria - I decided to <a>trade the city life</a> for nature and moved 
-        to a small town in the Austrian Alps
-        <span class="icon icon-txt-align">
-          <img class="" src="~/assets/icons/mountain.svg" />
-        </span> 
-      </div>
-      <div>&nbsp;</div>
-      <div class="columns is-vertical-center">
-        <div class="column is-two-fifths">
-          <figure class="image is-4by3">
-            <ImgModal v-bind:src="'/img/wien.jpg'"></ImgModal>
-          </figure>
+      <div class="columns reverse-columns">
+        <div class="column is-three-quarters">
+          <h4 class="subtitle is-4">Hi, my name is Michael Bierbaumer</h4>
+          <div>
+            I'm a {{age.years()}} years old freelance software engineer, living in Austria right in the center of Europe
+            <span class="icon icon-txt-align">
+              <img class="icon-rounded" src="~/assets/icons/eu.svg" />
+            </span>
+          </div>
+          <div>
+            After living 11 years in Vienna - the capital city of Austria - I decided to trade the city life for nature, and moved
+            to a small town in the Austrian Alps
+            <span
+              class="icon icon-txt-align"
+            >
+              <img class src="~/assets/icons/mountain.svg" />
+            </span>
+          </div>
         </div>
-        <div class="column has-text-centered"><span class="is-size-3">versus</span></div>
-        <div class="column is-two-fifths">
-            <figure class="image is-4by3">
-            <ImgModal v-bind:src="'/img/aschach.jpg'"></ImgModal>
+        <div class="column">
+          <figure class="image is-128x128 h-center-content">
+            <ImgModal clazz="is-rounded" src="/img/micha-profile.png" />
           </figure>
         </div>
       </div>
-      <div>&nbsp;</div>
-      <div>
-
-          
-
+      <div class="columns">
+        <div class="column">[[ tech details]]</div>
       </div>
-
-
     </section>
   </div>
 </template>
 
 <script>
 import moment from "moment";
-import ImgModal from '~/components/ImgModal';
+import ImgModal from "~/components/ImgModal";
 
 export default {
-    components:{
-        ImgModal
-    },
+  components: {
+    ImgModal
+  },
   computed: {
     age: function() {
       let bday = moment("1987-09-25", "YYYY-MM-DD");
